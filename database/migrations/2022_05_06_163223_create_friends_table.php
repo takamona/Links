7ccb42f84ad4be389ce44d7cbe7179ecfbc6f839
TakahisaKobayashi <takavyd2720ray@docomo.ne.jp>
2022-05-06 16:51:09 +0900

    更新

---
+++ database/migrations/2022_05_06_163223_create_friends_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');//ユーザーID
+            $table->unsignedBigInteger('friend_id');//送る相手のID
+            
+            $table->Integer('status')->default(0);//0,1,2で表す。
+            $table->timestamps();
+            
+            // 外部キー制約
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unique(['user_id', 'friend_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
