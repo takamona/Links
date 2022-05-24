0af955ef1960d7e4f26b899295debbc5bcefca58
TakahisaKobayashi <takavyd2720ray@docomo.ne.jp>
2022-05-24 15:48:41 +0900

    レイアウト調整

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html lang="ja">
  <head>
-  <meta charset="UTF-8">
+  <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>プロフィール登録</title>
     <link rel="stylesheet" href="{{asset('css/topic_show.css')}}">
+    
+  <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
   </head>
   <body>
     <div class="po">
@@ -29,9 +34,9 @@
     </div>
     <div class="bar"></div>
     @if($topic->user->id === Auth::id())
-    <p><a href="/communities/{{$community->id}}/topics/{{$topic->id}}/edit">編集</a></p>
+    <p><a href="/communities/{{$community->id}}/topics/{{$topic->id}}/edit">編集ページへ</a></p>
      {!! Form::open(['route' => ['topics.destroy', 'id' => $community->id ,'topic' => $topic->id ],'method'=>'DELETE']) !!}
-      {!! Form::submit('削除', ['class' => 'btn btn-primary btn-block']) !!}
+      {!! Form::submit('削除する', ['class' => 'btn btn-primary btn-lg']) !!}
       {!! Form::close() !!}
     @endif
      <table class="show">
@@ -49,18 +54,20 @@
       </tr>
      </table>
      <h2>投稿一覧</h2>
+     <div class="border2">
      <ul class="post">
      @foreach($posts as $post)
      <li><img class="profile" src="{{asset('/uploads/' . $post->user->profile->image)}}"></li>
-     <li class="content">{{$post->content}}</li><li class="name">{{$post->user->name}}さん</li><li class="day"><li class="aaaa">{{$post->created_at}}</li></li>
+     <li class="content">{{$post->content}}</li><li class="name">{{$post->user->name}}さんのコメント</li><li class="day"><li class="border_">{{$post->created_at}}</li></li>
      @endforeach
      </ul>
+     </div>
   @if($participation->status === 1 )
       <form action="/posts" method="POST">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <input type="hidden" name="topic_id" value="{{$topic->id}}">
         <div class="name_empty"><textarea class="nametext" placeholder="記入" name="content"></textarea></div>
-        <input type="submit" value="コメント投稿">
+        <input type="submit" class="button_submit" value="コメント投稿">
       </form>
   @endif
     <!--<div class="topic">-->
