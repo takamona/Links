bf2e4bea697f058c1dca81352893cfbb466de5e5
TakashisaKobayashi <takavyd2720ray@docomo.ne.jp>
2022-11-18 22:38:02 +0900

    テスト

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,11 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Storage;
+
+
+
+
 
 class Controller extends BaseController
 {
