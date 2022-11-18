e9872ffa107c7689451882ed68d5dfcb5b058e32
TakashisaKobayashi <takavyd2720ray@docomo.ne.jp>
2022-11-18 22:25:01 +0900

    画像アップロード検証

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -164,15 +164,10 @@ class ProfilesController extends Controller
                 $target_path = public_path('uploads/');
                 // アップロード処理
                 $file->move($target_path, $image);
-                // S3用
-                $path = Storage::disk('s3')->putFile('/uploads', $file, 'public');
-                // パスから、最後の「ファイル名.拡張子」の部分だけ取得
-                $image = basename($path);
             }else{
                 // 画像を選択していなければ、画像ファイルは元の名前のまま
                 $image = $profile->image;
             }
-        
             //入力情報をもとにインスタンスのプロパティ変更
             $profile->gender = $gender;
             $profile->address = $address;
