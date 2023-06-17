a07eeefb8cf13387df09ba0c33d3a8f09b005093
TakashisaKobayashi <takavyd2720ray@docomo.ne.jp>
2023-06-17 16:50:15 +0900

    ログインJSファイルの修正

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,19 @@
-const button = document.getElementById('s1');
-button.addEventListener('click', () => {
-   const sinki = document.getElementsByClassName('sinki')[0];
-   sinki.style.visibility = "visible";
-}); 
+// const button = document.getElementById('s1');
+// button.addEventListener('click', () => {
+//    const sinki = document.getElementsByClassName('sinki')[0];
+//    sinki.style.visibility = "visible";
+// }); 
+
+
+const button = document.getElementsByClassName("sinkitouroku")[0];
+const sinki = document.getElementsByClassName("sinki")[0];
+document.addEventListener('DOMContentLoaded', function () {
+ sinki.style.visibility = "hidden";
+ button.addEventListener('click', () => {
+    if (sinki.style.visibility === "hidden") {
+      sinki.style.visibility = "visible";
+    } else {
+      sinki.style.visibility = "hidden";
+    }
+ })
+})
