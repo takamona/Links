3f86e84ae4462c3f6fb5e873b42d22c12baab4fb
TakashisaKobayashi <takavyd2720ray@docomo.ne.jp>
2023-06-29 23:07:34 +0900

    レスポンシブデザイン調整

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -2,6 +2,7 @@
 <html lang="ja">
  <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>{{$user->name}}さんのマイページ</title>
   <link rel="stylesheet" href="{{ asset('css/mypage.css')}}" type="text/css" />
 </head>
