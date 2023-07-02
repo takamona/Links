d82baf6376a852ca3f9966f25f429e66e3f30a68
TakashisaKobayashi <takavyd2720ray@docomo.ne.jp>
2023-07-02 21:46:23 +0900

    レスポンシブ対応マイページ

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -126,6 +126,7 @@
 </div>
 <div class="user_position">
 <p class="username_click">{{$user->name}}さん</p>
+<div class="wrap_original"></div>
 <script src="{{ asset('https://code.jquery.com/jquery-3.3.1.min.js')}}"></script>
 <script src="{{ asset('/js/mypage.js')}}"></script>
 <script src="{{ asset('/js/menu.js')}}"></script>
