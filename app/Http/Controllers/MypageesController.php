21be56318083e971ee311299688aa54afd73ff23
TakashisaKobayashi <takavyd2720ray@docomo.ne.jp>
2024-11-30 19:05:53 +0900

    news取得方法変更

--- app/Http/Controllers/MypageesController.php
+++ app/Http/Controllers/MypageesController.php
@@ -123,6 +123,21 @@ class MypageesController extends Controller
 
         $news = [];
         $items = $rss->channel->item;
+        
+        
+        
+        
+        
+        $guzzleClient = new Client([
+            'headers' => [
+                'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+            ],
+            'allow_redirects' => true, // リダイレクトの追跡
+        ]);
+        
+        
+        
+        
         $goutteClient = new GoutteClient();
 
         foreach ($items as $item) {
