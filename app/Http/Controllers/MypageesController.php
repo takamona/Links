95ecd792da973163917f3b333e5bea6b41f4e757
TakashisaKobayashi <takavyd2720ray@docomo.ne.jp>
2024-11-30 16:48:18 +0900

    news取得方法変更

--- app/Http/Controllers/MypageesController.php
+++ app/Http/Controllers/MypageesController.php
@@ -139,6 +139,7 @@ class MypageesController extends Controller
                 // og:image を優先して取得
                 if ($crawler->filter('meta[property="og:image"]')->count() > 0) {
                     $thumbnail = $crawler->filter('meta[property="og:image"]')->attr('content');
+                    var_dump($thumbnail);
                 } else {
                     // og:image がない場合、<img> タグから最初の画像を取得
                     $images = $crawler->filter('img')->each(function ($node) {
